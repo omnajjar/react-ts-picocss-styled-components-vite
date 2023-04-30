b642925c64cf1e55533838d0ef374dec63b70a55
Osama Najjar <osama.najjar.m@gmail.com>
2023-05-01 00:15:17 +0200

    feat: set home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,55 @@ const MainWrapper = styled.main`
 	margin-top: 100px;
 `;
 
-const HeaderWrapper = styled.h2`
+const CenteredTextWrapper = styled.div`
 	text-align: center;
 `;
 
-const TextWrapper = styled.p`
-  text-align: center;
-`;
-
 export default function App() {
-  return (
-    <>
-      <MainWrapper className='container'>
-        <HeaderWrapper>🔥 Vite + React + TypeScript + Pico CSS + Styled Components 🔥</HeaderWrapper>
-        <TextWrapper>
-          Are you starting a small react project, then this template is for you, powered by:
-        </TextWrapper>
-      </MainWrapper>
-      <ThemeSwitcher />
-    </>
-  );
+	return (
+		<>
+			<MainWrapper className='container'>
+				<CenteredTextWrapper>
+					<h2>🚀 Vite + React + TypeScript + Pico CSS + Styled Components 🔥</h2>
+					<hr></hr>
+				</CenteredTextWrapper>
+				<p>
+					A small react project starter, perfect for very small apps a beautiful minimal setup,
+					powered by:
+				</p>
+				<ul>
+					<li>
+						<a href='https://react.dev/' target='_blank' rel='noreferrer'>
+							{'React(Typescript) ✨'}
+						</a>
+					</li>
+					<li>
+						<a href='https://picocss.com/' target='_blank' rel='noreferrer'>
+							{'Pico CSS(Minimal CSS Framework) 🎨'}
+						</a>
+					</li>
+					<li>
+						<a href='https://styled-components.com/' target='_blank' rel='noreferrer'>
+							{'Styled Components 💅'}
+						</a>
+					</li>
+				</ul>
+				<p>And 🔋 batteries are included:</p>
+				<ul>
+					<li>Dark Theme By Default</li>
+					<li>
+						<a href='https://prettier.io/' target='_blank' rel='noreferrer'>
+							Prettier
+						</a>
+					</li>
+					<li>
+						<a href='https://eslint.org/' target='_blank' rel='noreferrer'>
+							Eslint
+						</a>
+					</li>
+				</ul>
+			</MainWrapper>
+			<ThemeSwitcher />
+		</>
+	);
 }
