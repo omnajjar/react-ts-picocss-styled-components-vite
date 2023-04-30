29f408a1d8fd9ee511181365750ea899b19863d5
Osama Najjar <osama.najjar.m@gmail.com>
2023-04-30 23:00:44 +0200

    feat: create theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <h1>Test!</h1>;
+	return (
+		<>
+			<main className='container'>
+				<hgroup>
+					<h1>Basic template</h1>
+					<h2>A basic custom template for Pico using only CSS custom properties (variables).</h2>
+				</hgroup>
+			</main>
+		</>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 // styles
 import '@picocss/pico';
@@ -9,6 +9,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider defaultTheme={'dark'}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
